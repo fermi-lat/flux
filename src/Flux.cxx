863cbddcf2c19c74e0eded47332c46199d65c230
burnett <>
2003-08-28 18:31:12 +0000

    remove "local" time.

--- src/Flux.cxx
+++ src/Flux.cxx
@@ -1,7 +1,7 @@
 /** @file Flux.cxx
     @brief Implementation of Flux
 
-   $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Flux.cxx,v 1.28 2003/03/20 19:55:32 burnett Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/flux/src/Flux.cxx,v 1.1.1.1 2003/07/29 18:22:19 burnett Exp $
 
   Original author: T. Burnett
 */
@@ -11,8 +11,7 @@
 #include "flux/SpectrumFactory.h"
 
 Flux::Flux(std::string name) 
-: m_time(0)
-, m_flux(0)
+:  m_flux(0)
 {
     m_event = s_mgr->source(name);
 }
@@ -39,12 +38,11 @@ std::string Flux::title()const
 
 void Flux::generate()
 {
-    // Purpose and Method: generate a new entry trajectory, set FluxSource, increment local time
+    // Purpose and Method: generate a new entry trajectory, set FluxSource, time
     // Inputs  - none
     // Outputs - none
     m_flux = m_event->event(time());
     double timepass = m_event->interval(time());
-    m_time+= timepass;
     pass(timepass);
 }
 
@@ -67,7 +65,7 @@ HepPoint3D Flux::launchPoint()const
 
 double Flux::time()const 
 {
-    return m_time ;
+    return s_mgr->time();
 }
 
 
