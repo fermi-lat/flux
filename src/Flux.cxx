2af77ed52810ffaf5acfbf6877ccdf50c63068df
cvs2git <>
2003-10-21 13:58:08 +0000

    This commit was manufactured by cvs2svn to create tag 'v8r2'.
    Sprout from GLAST 2003-10-21 13:58:06 UTC burnett 'AutoUpdate'
    Cherrypick from master 2003-10-21 13:58:07 UTC burnett 'tagging v8r2':
        cmt/requirements
        doc/release.notes
        flux/CompositeSource.h
        flux/EventSource.h
        flux/Flux.h
        flux/FluxMgr.h
        flux/FluxSource.h
        flux/GPS.h
        flux/rootplot.h
        src/CHIMESpectrum.cxx
        src/CompositeSource.cxx
        src/Flux.cxx
        src/FluxMgr.cxx
        src/FluxSource.cxx
        src/GPS.cxx
        src/Geomag.cxx
        src/VdgGamma.cxx
        src/rootplot/rootplot.cxx
    Delete:
        src/CompositeSource.h

--- src/Flux.cxx
+++ src/Flux.cxx
@@ -1,7 +1,7 @@
 /** @file Flux.cxx
     @brief Implementation of Flux
 
-   $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/Flux.cxx,v 1.28 2003/03/20 19:55:32 burnett Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/flux/src/Flux.cxx,v 1.3 2003/10/01 22:21:50 srobinsn Exp $
 
   Original author: T. Burnett
 */
@@ -11,10 +11,14 @@
 #include "flux/SpectrumFactory.h"
 
 Flux::Flux(std::string name) 
-: m_time(0)
-, m_flux(0)
+:  m_flux(0)
 {
     m_event = s_mgr->source(name);
+}
+Flux::Flux(std::vector<std::string> names) 
+:  m_flux(0)
+{
+    m_event = s_mgr->compositeSource(names);
 }
 Flux::~Flux() 
 {
@@ -39,13 +43,14 @@ std::string Flux::title()const
 
 void Flux::generate()
 {
-    // Purpose and Method: generate a new entry trajectory, set FluxSource, increment local time
+    // Purpose and Method: generate a new entry trajectory, set FluxSource, time
     // Inputs  - none
     // Outputs - none
+	do{
     m_flux = m_event->event(time());
     double timepass = m_event->interval(time());
-    m_time+= timepass;
     pass(timepass);
+	}while(m_event->occulted());
 }
 
 // the particle generated 
@@ -67,7 +72,7 @@ HepPoint3D Flux::launchPoint()const
 
 double Flux::time()const 
 {
-    return m_time ;
+    return s_mgr->time();
 }
 
 
