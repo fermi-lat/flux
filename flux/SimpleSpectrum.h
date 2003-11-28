7046777e8b20d1a78f78bad11a9243cf0011c30f
cvs2git <>
2003-11-28 20:58:11 +0000

    This commit was manufactured by cvs2svn to create tag 'v8r3p3'.
    Sprout from GLAST 2003-11-28 20:58:09 UTC srobinsn 'AutoUpdate'
    Cherrypick from master 2003-11-28 20:58:10 UTC srobinsn 'tag ready':
        cmt/requirements
        doc/multipower.htm
        doc/multipower1.JPG
        doc/multipower4.JPG
        doc/release.notes
        flux/CompositeSource.h
        flux/EventSource.h
        flux/Flux.h
        flux/FluxMgr.h
        flux/FluxSource.h
        flux/GPS.h
        flux/IFlux.h
        flux/SimpleSpectrum.h
        flux/rootplot.h
        src/CHIMESpectrum.cxx
        src/CompositeSource.cxx
        src/Flux.cxx
        src/FluxMgr.cxx
        src/FluxSource.cxx
        src/GPS.cxx
        src/Geomag.cxx
        src/SimpleSpectrum.cxx
        src/VdgGamma.cxx
        src/mainpage.h
        src/rootplot/rootplot.cxx
        xml/source.dtd
        xml/source_library.xml
    Delete:
        doc/used_packages.html
        src/CompositeSource.h

--- flux/SimpleSpectrum.h
+++ flux/SimpleSpectrum.h
@@ -1,7 +1,7 @@
 /** @file SimpleSpectrum.h
     @brief declaration of SimpleSpectrum
 
-   $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/SimpleSpectrum.h,v 1.6 2003/04/03 19:44:50 burnett Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/flux/flux/SimpleSpectrum.h,v 1.2 2003/10/29 00:58:47 burnett Exp $
 */
 #ifndef SIMPLESPECTRUM_H
 #define SIMPLESPECTRUM_H
@@ -16,28 +16,34 @@ class DOM_Element;
 * \class SimpleSpectrum
 * @brief define a particle and spectral index
 * 
-* $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/SimpleSpectrum.h,v 1.6 2003/04/03 19:44:50 burnett Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/flux/flux/SimpleSpectrum.h,v 1.2 2003/10/29 00:58:47 burnett Exp $
 */
 class SimpleSpectrum : public Spectrum {
 public: 
-    SimpleSpectrum(const char* name,float E0, float index=0.0);
-    SimpleSpectrum(const char* name,float Emin, float Emax, float index);
+
+    /// ctor for instantiation from XML element  "particle"
+    /// @param xelem nested element, expect either "power_law or "energy"
     SimpleSpectrum(const DOM_Element& xelem, bool useGeV=true);
+
+    /// ctor for instantiation from XML element "SpectrumClass"
+    /// @param parameter string 
+    /// This is required, but the implementation has never been used
     SimpleSpectrum(const std::string& params);
     
-    SimpleSpectrum();
-    void setPosition ( float /*lat*/, float /*lon*/ ){}
-    virtual double calculate_rate(double old_rate);
     virtual float  operator()(float f)const;
     virtual const char* particleName()const;
     virtual std::string title()const;
+
 private:
     float parseParamList(std::string input, int index);
     float m_E0;		// energy base
     std::string m_name;	// particle name to generate ("P", "gamma", ...)
     float m_index;	// spectral index: <=1 is delta function at E0
     float m_emax;
+    float m_ebreak;    // if not zero, put in a break
+    float m_index2;   // index for break
     bool m_useGeV;  // true if using GeV units, MeV otherwise
+    double m_a;  // relative area of lower part of broken spectrum
 };
 
 #endif
