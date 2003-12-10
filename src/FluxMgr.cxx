20aaa116d04685019e3ac852334c819ad2cd7c64
jrb <>
2003-12-10 21:01:15 +0000

    Substitute call to facilities::Util::expandEnvVar for xml::IFile::extractEnvVar since the latter will someday disappear.

--- src/FluxMgr.cxx
+++ src/FluxMgr.cxx
@@ -1,7 +1,7 @@
 /** @file FluxMgr.cxx
     @brief Implementation of FluxMgr
 
-  $Header: /nfs/slac/g/glast/ground/cvs/flux/src/FluxMgr.cxx,v 1.7 2003/11/03 09:41:21 srobinsn Exp $
+  $Header: /nfs/slac/g/glast/ground/cvs/flux/src/FluxMgr.cxx,v 1.8 2003/11/07 15:17:27 burnett Exp $
 */
 
 #include "flux/FluxMgr.h"
@@ -14,7 +14,7 @@
 #include <xercesc/dom/DOM_Document.hpp>
 #include <xercesc/dom/DOM_Element.hpp>
 #include "xml/Dom.h"
-#include "xml/IFile.h"
+#include "facilities/Util.h"     // for expandEnvVar
 
 #include "astro/PointingTransform.h"
 
@@ -444,7 +444,7 @@ std::string FluxMgr::writeXmlFile(const std::vector<std::string>& fileList) {
     //the default DTD file
     inFileName=m_dtd;
     //replace $(FLUXROOT) by its system variable
-    xml::IFile::extractEnvVar(&inFileName);
+    facilities::Util::expandEnvVar(&inFileName);
     
     //this stuff goes in the beginnning of the XML file to be read into the parser
     fileString << "<?xml version='1.0' ?>" << std::endl << "<!DOCTYPE source_library" 
@@ -455,7 +455,7 @@ std::string FluxMgr::writeXmlFile(const std::vector<std::string>& fileList) {
         
         // get the file name, and evaluate any system variables in it
         inFileName=(*iter).c_str();
-        xml::IFile::extractEnvVar(&inFileName);
+        facilities::Util::expandEnvVar(&inFileName);
         
         //then make an ENTITY entry specifying where the file is
         fileString << "<!ENTITY " << "library" << libchar << " SYSTEM " << '"' 
