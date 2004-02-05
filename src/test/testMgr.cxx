3d00efe09090e11ac5f02353d405adfc6b044a9b
srobinsn <>
2004-02-05 23:15:02 +0000

    equatorial/celestial fix for new astro package.

--- src/test/testMgr.cxx
+++ src/test/testMgr.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/flux/src/test/testMgr.cxx,v 1.6 2004/01/26 23:19:40 srobinsn Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/flux/src/test/testMgr.cxx,v 1.7 2004/01/28 23:52:25 hierath Exp $
 
 //#include "FluxSvc/ISpectrumFactory.h"
 
@@ -75,7 +75,7 @@ void galacticTest(FluxMgr* fm, std::string sourceName,double count){
         time+=interval;
         fm->pass(interval);
         Hep3Vector abc(fm->transformToGlast(time,GPS::CELESTIAL).inverse()*(-(f->launchDir())));
-        astro::SkyDir dir(abc,astro::SkyDir::CELESTIAL);
+        astro::SkyDir dir(abc,astro::SkyDir::EQUATORIAL);
         std::cout << "particle "<< i << " located at (l,b) = " << dir.l() << "," << dir.b() << std::endl;
         lavg +=dir.l();
         bavg +=dir.b();
