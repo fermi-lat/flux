9ba0b3231717f10a6964afd1dff03fbaefccdcfb
cvs2git <>
2004-02-06 21:44:02 +0000

    This commit was manufactured by cvs2svn to create tag 'v8r6p2'.
    Sprout from GLAST 2004-02-06 21:44:01 UTC hierath 'AutoUpdate'
    Cherrypick from master 2004-02-06 21:44:00 UTC hierath '':
        cmt/requirements
        doc/multipower.htm
        doc/multipower1.JPG
        doc/multipower4.JPG
        doc/release.notes
        flux/AGNSpectrum.h
        flux/CompositeSource.h
        flux/EventSource.h
        flux/Flux.h
        flux/FluxMgr.h
        flux/FluxSource.h
        flux/IFlux.h
        flux/MapSpectrum.h
        flux/SimpleSpectrum.h
        flux/rootplot.h
        sources/gas_gal.fits
        src/AGNSpectrum.cxx
        src/AlbedoPSpectrum.cxx
        src/CHIMESpectrum.cxx
        src/CompositeSource.cxx
        src/Flux.cxx
        src/FluxMgr.cxx
        src/FluxSource.cxx
        src/GalElSpectrum.cxx
        src/Geomag.cxx
        src/MapSpectrum.cxx
        src/SimpleSpectrum.cxx
        src/VdgGamma.cxx
        src/mainpage.h
        src/rootplot/rootplot.cxx
        src/test/testMgr.cxx
        xml/3EG.xml
        xml/source.dtd
        xml/source_library.xml
    Delete:
        doc/used_packages.html
        flux/GPS.h
        src/CompositeSource.h
        src/GPS.cxx

--- src/CHIMESpectrum.cxx
+++ src/CHIMESpectrum.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/CHIMESpectrum.cxx,v 1.14 2003/02/23 02:08:22 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/flux/src/CHIMESpectrum.cxx,v 1.3 2003/10/02 01:10:14 hierath Exp $
 
 
 #include "CHIMESpectrum.h"
@@ -9,7 +9,7 @@
 #include <algorithm>
 #include <functional>
 #include "CLHEP/Random/Random.h"
-#include "flux/GPS.h"
+#include "astro/GPS.h"
 
 // this is needed to include in the executable or dll
 #include "flux/SpectrumFactory.h"
@@ -301,7 +301,7 @@ std::pair<double,double> CHIMESpectrum::dir(double energy)
     int trial=0;
     do {
         // uniform distribution within Stormer cone
-        cospolar = -1. + HepRandom::getTheGenerator()->flat()*(cosomega(energy)+1.);
+        cospolar = -1. + HepRandom::getTheGenerator()->flat()*(cosomega(energy*0.001)+1.);
         sinpolar = sqrt(1.-cospolar*cospolar);
         azi = 2.*M_PI* HepRandom::getTheGenerator()->flat();
         coszenith = cos(azi)*sinpolar;
