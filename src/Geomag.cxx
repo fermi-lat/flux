8b8efebf9cccfb39eef5089d1328b41c674c2947
cvs2git <>
2004-02-13 22:15:15 +0000

    This commit was manufactured by cvs2svn to create tag 'v8r7'.
    Sprout from GLAST 2004-02-06 21:44:01 UTC hierath 'AutoUpdate'
    Cherrypick from master 2004-02-13 22:15:14 UTC burnett 'tagging v8r7':
        cmt/requirements
        doc/multipower.htm
        doc/multipower1.JPG
        doc/multipower4.JPG
        doc/release.notes
        flux/AGNSpectrum.h
        flux/CompositeSource.h
        flux/EventSource.h
        flux/Flux.h
        flux/FluxMgr.h
        flux/FluxSource.h
        flux/IFlux.h
        flux/MapSpectrum.h
        flux/SimpleSpectrum.h
        flux/rootplot.h
        sources/gas_gal.fits
        src/AGNSpectrum.cxx
        src/AlbedoPSpectrum.cxx
        src/CHIMESpectrum.cxx
        src/CompositeSource.cxx
        src/Flux.cxx
        src/FluxMgr.cxx
        src/FluxSource.cxx
        src/GalElSpectrum.cxx
        src/Geomag.cxx
        src/MapSpectrum.cxx
        src/SimpleSpectrum.cxx
        src/VdgGamma.cxx
        src/mainpage.h
        src/rootplot/rootplot.cxx
        src/test/testMgr.cxx
        xml/3EG.xml
        xml/source.dtd
        xml/source_library.xml
    Delete:
        doc/used_packages.html
        flux/GPS.h
        src/CompositeSource.h
        src/GPS.cxx

--- src/Geomag.cxx
+++ src/Geomag.cxx
@@ -1,4 +1,5 @@
 #include "Geomag.h"
+#include <iostream>
 
 #include "Geomag.inc"
 
@@ -34,11 +35,25 @@ double Geomag::geolon(std::pair<double,double>coords) {
     return Geomag::geolon(coords.first, coords.second);
 }
 
+// Allowed Latitude range is -30 to +30
+// Allowed Longitude range is -180 to 180 (but 0 to 360 works too)
 double Geomag::geoInterp(double lat, double lon, const double * array) {
-    int ilat = static_cast<int>(lat/5.+6);
-    int ilon = static_cast<int>(lon/5.);
-    double a = fmod(lat+30., 5.)/5.;
-    double b = fmod(lon, 5.)/5.;
+   int ilat, ilon;
+   double a, b;
+
+   if(lon < 0)
+      lon += 360;
+
+    ilat = static_cast<int>(lat/5.+6);
+    ilon = static_cast<int>(lon/5.);
+    a = fmod(lat+30., 5.)/5.;
+    b = fmod(lon, 5.)/5.;
+
+	if(abs(lat) > 30)
+	{
+		std::cout << "Warning -- Geomag::geoInterp -- Latitude is out of range."  << std::endl;
+	}
+
     return array[ilat   + 13*ilon    ] * (1.-a) * (1.-b) +
         array[ilat   + 13*(ilon+1)] * (1.-a) * b      +
         array[ilat+1 + 13*ilon    ] * a      * (1.-b) +
