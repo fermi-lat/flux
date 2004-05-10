f6d3f587bdc2041855816ca37bcf14bc5682ef40
burnett <>
2004-05-10 18:42:33 +0000

    tagging p4 after cleanup, restore of VdgGamma

--- src/FluxMgr.cxx
+++ src/FluxMgr.cxx
@@ -1,7 +1,7 @@
 /** @file FluxMgr.cxx
-    @brief Implementation of FluxMgr
+@brief Implementation of FluxMgr
 
-  $Header: /nfs/slac/g/glast/ground/cvs/flux/src/FluxMgr.cxx,v 1.14 2004/01/28 23:52:25 hierath Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/flux/src/FluxMgr.cxx,v 1.15 2004/03/17 01:10:35 jrb Exp $
 */
 
 #include "flux/FluxMgr.h"
@@ -20,7 +20,6 @@
 
 #include <sstream>
 #include <map>
-#define DLL_DECL_SPECTRUM(x)   extern const ISpectrumFactory& x##Factory; x##Factory.addRef();
 #define DECLARE_SPECTRUM(x)   extern const ISpectrumFactory& x##Factory; x##Factory.addRef();
 
 
@@ -48,79 +47,70 @@ void FluxMgr::defaultFile(){
 }
 
 void FluxMgr::init(const std::vector<std::string>& fileList){	
-    
+
     //Purpose:  to initialize the infrastructure of the package with
     //the named xml files.
     std::string fileName;
-    
+
     xml::XmlParser parser;
-    
+
     std::string xmlFileIn = writeXmlFile(fileList);
-    
+
     // a quick way of displaying what goes to the parser
     //std::cout << xmlFileIn <<std::endl;
-    
+
     m_library_doc = parser.parse(xmlFileIn);
-    
+
     if (m_library_doc == DOM_Document()) {
         FATAL_MACRO("Parse error: processing the document" << std::endl
             << xmlFileIn << std::endl);
         return;
     }
-    
+
     // Root element is of type source_library.  Content is
     // one or more source elements.
-    
+
     s_library = m_library_doc.getDocumentElement();
-    
+
     // loop through the source elements to create a map of names, DOM_Elements
     if (s_library != DOM_Element()) {
-        
+
         DOM_Element child = xml::Dom::getFirstChildElement(s_library);
         DOM_Element toplevel = xml::Dom::getFirstChildElement(s_library);
-        
+
         while (child != DOM_Element()) {
-#if 0
-            while (child.getAttribute("name") == DOMString())
-#else
             while (!(xml::Dom::hasAttribute(child, "name"))  )
-#endif
             {
                 s_library = child;
                 child = xml::Dom::getFirstChildElement(s_library);
             }
-            
+
             while (child != DOM_Element()) {
-#if 0
-                std::string name = xml::Dom::transToChar(child.getAttribute("name"));
-                //std::cout << name << std::endl;
-                std::string parentfilename = xml::Dom::transToChar(toplevel.getAttribute("title"));
-#else
                 std::string name = xml::Dom::getAttribute(child, "name");
                 std::string parentfilename = xml::Dom::getAttribute(toplevel, "title");
-#endif
                 m_sources[name]=std::make_pair<DOM_Element,std::string>(child,parentfilename);
                 child = xml::Dom::getSiblingElement(child);
             }
-            
+
             child = xml::Dom::getSiblingElement(toplevel);
             toplevel=child;
         }
-        
+
     }
     // these are the locally defined spectra that we want to make available
-    DLL_DECL_SPECTRUM( FILESpectrum);
-    DLL_DECL_SPECTRUM( TimeCandle);
+    DECLARE_SPECTRUM( FILESpectrum);
+    DECLARE_SPECTRUM( TimeCandle);
 
-    DLL_DECL_SPECTRUM( SurfaceMuons);
+    DECLARE_SPECTRUM( SurfaceMuons);
+    DECLARE_SPECTRUM( VdgGamma);
 
     // these are deprecated, will be replaced by Hiroshima group
-    DLL_DECL_SPECTRUM( AlbedoPSpectrum);
-    DLL_DECL_SPECTRUM( CHIMESpectrum );
-    DLL_DECL_SPECTRUM( GalElSpectrum);
-    DLL_DECL_SPECTRUM( MapSpectrum);
-    DLL_DECL_SPECTRUM( AGNSpectrum);
-    
+    DECLARE_SPECTRUM( AlbedoPSpectrum);
+    DECLARE_SPECTRUM( CHIMESpectrum );
+    DECLARE_SPECTRUM( GalElSpectrum);
+    DECLARE_SPECTRUM( MapSpectrum);
+    DECLARE_SPECTRUM( AGNSpectrum);
+
 }
 
 
@@ -163,60 +153,42 @@ EventSource*  FluxMgr::getSourceFromXML(const DOM_Element& src)
 
     DOM_Node    childNode = src.getFirstChild();
     if (childNode == DOM_Node()) {
-    /*
-    FATAL_MACRO("Improperly formed XML event source");
-    return 0;
+        /*
+        FATAL_MACRO("Improperly formed XML event source");
+        return 0;
         */
         // no child node: expect to find the name defined.
         return  new FluxSource(src);
     }
-    
+
     DOM_Element sname = xml::Dom::getFirstChildElement(src);
     if (sname == DOM_Element() ) {
         FATAL_MACRO("Improperly formed XML event source");
         return 0;
     }
     // If we got here, should have legit child element
-#if 0
-    if ((sname.getTagName()).equals("spectrum"))
-#else
     if (xml::Dom::checkTagName(sname, "spectrum") )
-#endif
     {
         return  new FluxSource(src);
     }
-#if 0
-    else if ((sname.getTagName()).equals("nestedSource")) 
-#else
     else if (xml::Dom::checkTagName(sname, "nestedSource"))
-#endif
     {
-        
+
         // Search for and process immediate child elements.  All must
         // be of type "nestedSource".  There may be more than one.
         // Content model for nestedSource is EMPTY, so can omit check
         // for that in the code
-        
+
         CompositeSource* cs;
-            cs = new CompositeSource();
+        cs = new CompositeSource();
         do { 
             DOM_Element selem = 
-#if 0
-                getLibrarySource(sname.getAttribute("sourceRef"));
-#else
                 getLibrarySource(xml::Dom::getAttribute(sname, "sourceRef"));
-#endif
 
             if (selem == DOM_Element()) {
-#if 0
-                FATAL_MACRO("source name" << 
-                    xml::Dom::transToChar(sname.getAttribute("sourceRef")) << 
-                    "' not in source library");
-#else
                 FATAL_MACRO("source name" << 
                     xml::Dom::getAttribute(sname, "sourceRef") <<
                     "' not in source library");
-#endif
             }
             cs->addSource(getSourceFromXML(selem)); 
             sname = xml::Dom::getSiblingElement(sname);
@@ -225,71 +197,58 @@ EventSource*  FluxMgr::getSourceFromXML(const DOM_Element& src)
         return cs;
     }
     else {
-#if 0
-        FATAL_MACRO("Unexpected element: " << 
-            xml::Dom::transToChar(sname.getTagName()) );
-#else 
         FATAL_MACRO("Unexpected element: " << 
             xml::Dom::getTagName(sname) );
-#endif
     }
     return 0;
 }
 
 
 
-#if 0
-DOM_Element    FluxMgr::getLibrarySource(const DOMString& id)
-#else
 DOM_Element    FluxMgr::getLibrarySource(const std::string& id)
-#endif
 {
     //Purpose: source library lookup.  Each source is uniquely identified
     // by its "name" attribute because "name" is of type ID
-    
+
     // quit if the library was unitialized
     if (s_library == DOM_Element() ) return DOM_Element(); 
-    
-#if 0
-    return m_library_doc.getElementById(id);
-#else
+
     return xml::Dom::getElementById(m_library_doc, id);
-#endif
 }
 
 std::list<std::string> FluxMgr::sourceList() const
 {
     std::list<std::string> s;
     for( std::map<std::string, std::pair<DOM_Element,std::string> >::const_iterator it = m_sources.begin();
-    it != m_sources.end();
-    ++it){
-        s.push_back((*it).first);
-    }
-    return s;
+        it != m_sources.end();
+        ++it){
+            s.push_back((*it).first);
+        }
+        return s;
 }
 
 std::vector<std::pair< std::string ,std::list<std::string> > > FluxMgr::sourceOriginList() const
 {
     std::vector<std::pair< std::string ,std::list<std::string> > > originList;
     for( std::map<std::string, std::pair<DOM_Element,std::string> >::const_iterator it = m_sources.begin();
-    it != m_sources.end();
-    ++it){
-        //now see if we can find the filename already used in the vector:
-        std::vector<std::pair< std::string ,std::list<std::string> > >::iterator topiter;
-        for(topiter = originList.begin() ; topiter != originList.end() ; topiter++){
-            if( (*topiter).first==(*it).second.second) break;
+        it != m_sources.end();
+        ++it){
+            //now see if we can find the filename already used in the vector:
+            std::vector<std::pair< std::string ,std::list<std::string> > >::iterator topiter;
+            for(topiter = originList.begin() ; topiter != originList.end() ; topiter++){
+                if( (*topiter).first==(*it).second.second) break;
+            }
+
+            if( topiter != originList.end() ){ (*topiter).second.push_back((*it).first);
+            }else{
+                std::list<std::string> abc;
+                abc.push_back((*it).first);
+                originList.push_back(std::make_pair< std::string ,std::list<std::string> >((*it).second.second,abc) );
+            }
+
         }
 
-        if( topiter != originList.end() ){ (*topiter).second.push_back((*it).first);
-        }else{
-            std::list<std::string> abc;
-            abc.push_back((*it).first);
-            originList.push_back(std::make_pair< std::string ,std::list<std::string> >((*it).second.second,abc) );
-        }
-
-    }
-
-    return originList;
+        return originList;
 }
 
 /// generate some test output
@@ -298,32 +257,32 @@ void FluxMgr::test(std::ostream& cout, std::string source_name, int count)
     EventSource* e = source(source_name);
     setExpansion(1.);
     double time=0.;
-    
+
     const int howMany = e->howManySources();
     std::map<int,int> counts;
-    
-    
+
+
     cout << "running source: " << e->fullTitle() << std::endl;
     cout << " Total rate is: " << e->rate(time) << " Hz into " << e->totalArea() << " m^2" << std::endl;
     //cout << "LaunchType" << f->retLaunch() << "Pointtype" << f->retPoint() <<std::endl;
     cout << " there are " << howMany << " Sources total..." << std::endl;
     cout << "    Generating " << count << " trials " << std::endl;
     cout << " --------------------------------" << std::endl;
-    
+
     //testing rotateangles function
     GPS::instance()->rotateAngles(std::make_pair<double,double>(0.0,0.0));
     EventSource* f;
     double totalinterval=0;
     for( int i = 0; i< count; ++i) {
-        
+
         f = e->event(time);
         //TESTING THE lat, lon FUNCTIONS
         //cout << std::endl << "lat=" << GPS::instance()->lat() << ' ' <<"lon=" << GPS::instance()->lon() << std::endl;
         //double curTime=GPS::instance()->time();
         //cout << std::endl << "testlat=" << GPS::instance()->orbit()->testLatitude(curTime) << ' ' << "testlon=" << GPS::instance()->orbit()->testLongitude(curTime) << std::endl;
-        
+
         double interval=e->interval(time);
-        
+
         //here we increment the "elapsed" time and the "orbital" time,
         //just as is done in flux.  NOTE: this is important for the operation 
         //of fluxsource, and is expected.
@@ -332,7 +291,7 @@ void FluxMgr::test(std::ostream& cout, std::string source_name, int count)
         int sourceNumber = e->numSource();
         if(sourceNumber==-1){counts[0]++;
         }else{counts[sourceNumber]++;}
-        
+
         totalinterval+=interval;
         cout << f->particleName()
             << "(" << f->energy()<< " GeV)"
@@ -345,16 +304,16 @@ void FluxMgr::test(std::ostream& cout, std::string source_name, int count)
             << std::endl;
     }
     cout << "------------------------------------------------------" << std::endl;
-    
+
     cout << std::endl << "Average Interval=" << totalinterval/count <<" , "
         << "Average rate = " << count/totalinterval <<std::endl;
-    
+
     cout << "Source Statistics: " << std::endl;
     for( std::map<int,int>::const_iterator q=counts.begin() ; q!= counts.end() ; ++q){
         cout << "source #" << q->first << ": " << q->second << " events counted." << std::endl;
     }
-    
-    
+
+
 }
 
 
@@ -372,7 +331,7 @@ std::pair<double,double> FluxMgr::getExplicitRockingAngles(){
 
 /// set the desired pointing history file to use:
 void FluxMgr::setPointingHistoryFile(std::string fileName){
-	GPS::instance()->setPointingHistoryFile(fileName);
+    GPS::instance()->setPointingHistoryFile(fileName);
 }
 
 void FluxMgr::setExpansion (double p){
@@ -415,20 +374,10 @@ HepRotation FluxMgr::CELTransform(double time){
 
 //get the transformation matrix.
 HepRotation FluxMgr::orientTransform(double time){
-	//make the transformtion that turns zenith coordinates into local coordinates.
+    //make the transformtion that turns zenith coordinates into local coordinates.
     HepRotation ret;
     ret = GPS::instance()->transformToGlast(time,GPS::ZENITH);
-	//note:  this transformation is only used by FluxDisplay to tell where the earth's horizon is.
-	//it will rotate zenith coordinates into a frame where the "upwards direction" becomes the direction of the
-	//zenith in spacecraft coordinates, but is not more specific than that.
-	//astro::SkyDir dirZ( GPS::instance()->RAZ() , GPS::instance()->DECZ() );
-	//astro::SkyDir dirX( GPS::instance()->RAX() , GPS::instance()->DECX() );
-	//astro::SkyDir dirZenith( GPS::instance()->RAZenith() , GPS::instance()->DECZenith() );
-	//astro::PointingTransform point(dirZ,dirX);
-	//Hep3Vector localZenith((point.localToCelestial().inverse())*dirZenith());
-	//Hep3Vector perp1(localZenith.orthogonal());
-	//HepRotation ret(perp1,localZenith.cross(perp1),localZenith);
-	return ret;
+    return ret;
 }
 
 ///this transforms glast-local (cartesian) vectors into galactic (cartesian) vectors
@@ -438,79 +387,79 @@ HepRotation FluxMgr::transformGlastToGalactic(double time){
 
 ///this sets the rocking mode in GPS.
 std::vector<double> FluxMgr::setRockType(GPS::RockType rockType, double rockAngle){
-   int type=GPS::instance()->setRockType(rockType);
-   double degrees = GPS::instance()->rockingDegrees(rockAngle);
-   std::vector<double> ret;
-   ret.push_back(type);
-   ret.push_back(degrees);
-   return ret;
+    int type=GPS::instance()->setRockType(rockType);
+    double degrees = GPS::instance()->rockingDegrees(rockAngle);
+    std::vector<double> ret;
+    ret.push_back(type);
+    ret.push_back(degrees);
+    return ret;
 }
 
 ///this sets the rocking mode in GPS.
 std::vector<double> FluxMgr::setRockType(int rockType, double rockAngle){
-   int type=GPS::instance()->setRockType(rockType);
-   double degrees = GPS::instance()->rockingDegrees(rockAngle);
-   std::vector<double> ret;
-   ret.push_back(type);
-   ret.push_back(degrees);
-   return ret;
+    int type=GPS::instance()->setRockType(rockType);
+    double degrees = GPS::instance()->rockingDegrees(rockAngle);
+    std::vector<double> ret;
+    ret.push_back(type);
+    ret.push_back(degrees);
+    return ret;
 }
 
 std::string FluxMgr::writeXmlFile(const std::vector<std::string>& fileList) {
-/** purpose: creates a document of the form
+    /** purpose: creates a document of the form
 
-  <?xml version='1.0' ?>
-  <!DOCTYPE source_library SYSTEM "d:\users\burnett\pdr_v7r1c\flux\v5r3/xml/source.dtd" [
-  <!ENTITY librarya SYSTEM "d:\users\burnett\pdr_v7r1c\flux\v5r3/xml/user_library.xml" >
-  <!ENTITY libraryb SYSTEM "d:\users\burnett\pdr_v7r1c\flux\v5r3/xml/source_library.xml" >
-  ]>
-  <source_library>
-  &librarya;
-  &libraryb;
-  </source_library>
-  
+    <?xml version='1.0' ?>
+    <!DOCTYPE source_library SYSTEM "d:\users\burnett\pdr_v7r1c\flux\v5r3/xml/source.dtd" [
+    <!ENTITY librarya SYSTEM "d:\users\burnett\pdr_v7r1c\flux\v5r3/xml/user_library.xml" >
+    <!ENTITY libraryb SYSTEM "d:\users\burnett\pdr_v7r1c\flux\v5r3/xml/source_library.xml" >
+    ]>
+    <source_library>
+    &librarya;
+    &libraryb;
+    </source_library>
+
     */
     std::stringstream fileString;
     // Unique tag to add to ENTITY elements in the DTD.
     char libchar = 'a';
     std::string inFileName;
-    
+
     std::vector<std::string>::const_iterator iter = fileList.begin();
-    
+
     //the default DTD file
     inFileName=m_dtd;
     //replace $(FLUXROOT) by its system variable
     facilities::Util::expandEnvVar(&inFileName);
-    
+
     //this stuff goes in the beginnning of the XML file to be read into the parser
     fileString << "<?xml version='1.0' ?>" << std::endl << "<!DOCTYPE source_library" 
         << " SYSTEM " << '"' << inFileName << '"' << " [" << std::endl;
-    
+
     //as long as there are files in the file list...
     for (;iter != fileList.end(); iter++) {
-        
+
         // get the file name, and evaluate any system variables in it
         inFileName=(*iter).c_str();
         facilities::Util::expandEnvVar(&inFileName);
-        
+
         //then make an ENTITY entry specifying where the file is
         fileString << "<!ENTITY " << "library" << libchar << " SYSTEM " << '"' 
             << inFileName << "\" >" << std::endl;      
         libchar++;
     }
-    
+
     fileString << "]>" << std::endl << "<source_library>" << std::endl;
     iter = fileList.begin();
     libchar = 'a';
-    
+
     //as long as there are files in the file list...
     for (;iter != fileList.end(); iter++) {
         // add a reference to the file name
         fileString << "&library" << libchar << ";" << std::endl;       
         libchar++;
     }
-    
+
     fileString << "</source_library>" << '\0';
     return fileString.str();
-    
+
 }
