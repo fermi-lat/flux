7a7a16d029ac130b9885dc9d657314f1f0ebdd7a
burnett <>
2005-04-26 17:19:35 +0000

    clean up interface a bit, add name access

--- flux/SpectrumFactory.h
+++ flux/SpectrumFactory.h
@@ -1,23 +1,24 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/FluxSvc/src/SpectrumFactory.h,v 1.4 2002/07/23 19:00:57 srobinsn Exp $
-
-#if !defined(AFX_SPECTRUMFACTORY_H__211C2F25_9111_44B9_B357_0762789222AF__INCLUDED_)
-#define AFX_SPECTRUMFACTORY_H__211C2F25_9111_44B9_B357_0762789222AF__INCLUDED_
 /** 
-* \class SpectrumFactory
+* @file SpectrumFactory.h
+* @brief declare SpectrumFactory
 *
-* \brief Template class designed to hold method by which to polymorphically instantiate new Spectrum objects 
-* 
-* $Header $
+*  $Header$
 */
-#if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
+
+#ifndef flux_SpectrumFactory_h
+#define flux_SpectrumFactory_h
 
 #include "flux/ISpectrumFactory.h"
 #include "SpectrumFactoryTable.h"
 #include <typeinfo>
 #include <vector>
 
+/** 
+* \class SpectrumFactory
+*
+* \brief Template class designed to hold method by which to polymorphically instantiate new Spectrum objects 
+* 
+*/
 
 template <class T> class SpectrumFactory : public ISpectrumFactory 
 {
@@ -25,20 +26,24 @@ public:
     
     SpectrumFactory(){
         //Get class name using RTTI:
-        std::string classname = typeid(T).name();
-        int s = classname.find_first_of("class");
+        m_classname = typeid(T).name();
+        int s = m_classname.find_first_of("class");
         if( s==0 ) s=6; //found it
-        else s =classname.find_first_not_of("0123456789");
-        classname = classname.substr(s);
-        SpectrumFactoryTable::instance()->addFactory(classname, this); 
+        else s =m_classname.find_first_not_of("0123456789");
+        m_classname = m_classname.substr(s);
+        SpectrumFactoryTable::instance()->addFactory(m_classname, this); 
     }
     //! return a new Spectrum object
     virtual ISpectrum* instantiate(const std::string& params) const{return new T(params);}
     
     //! dummy to follow Gaudi model
     virtual void addRef()const{}
+
+    std::string name()const{return m_classname;}
+private:
+    std::string m_classname;
 };
 
 
 
-#endif // !defined(AFX_SPECTRUMFACTORY_H__211C2F25_9111_44B9_B357_0762789222AF__INCLUDED_)
+#endif
